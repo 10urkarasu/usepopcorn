984dd6dc7947670aa2cd85e991355bbea55ffaa6
10urkarasu <10urkarasu@gmail.com>
2024-01-17 09:22:35 +0300

    file structure edited

---
+++ src/components/Search.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "react";
+
+export default function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) return;
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+  //   useEffect(function () {
+  //     const el = document.querySelector(".search");
+  //     console.log(el);
+  //     el.focus();
+  //   }, []);
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
+    />
+  );
+}
